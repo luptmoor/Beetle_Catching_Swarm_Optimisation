8c2414dc56b91dcbb0c482750852df94951f2527
luptmoor <luptmoor@student.tudelft.nl>
2023-07-06 16:29:52 +0200

    PhysicalObject.py outsourced

---
+++ PhysicalObject.py
@@ -0,0 +1,7 @@
+from settings import  *
+class PhysicalObject:
+    def __init__(self, name, x, y, r_col):
+        self.name = name
+        self.x = x
+        self.y = y
+        self.r_col = r_col
